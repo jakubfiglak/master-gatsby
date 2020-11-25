3c7c798a81ed8237c54f1cb3822ec96af62a728d
Jakub Figlak <jakub.figlak@gmail.com>
2020-11-25 22:12:13 +0100

    Lessons 21-25

---
+++ starter-files/gatsby/src/templates/Pizza.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const StyledPizzaGrid = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+const SinglePizzaPage = ({ data: { pizza } }) => {
+  console.log(pizza);
+
+  return (
+    <StyledPizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </StyledPizzaGrid>
+  );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      id
+      name
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        id
+        name
+        vegetarian
+      }
+    }
+  }
+`;
+
+export default SinglePizzaPage;
